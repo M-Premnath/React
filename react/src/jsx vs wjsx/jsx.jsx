e5c7e21f01853ffd0c125a78fe9703b418ed0fb0
Premnath M <128115285+M-Premnath@users.noreply.github.com>
2024-12-23 19:44:40 +0530

    Jsx vs Js

---
+++ react/src/jsx vs wjsx/jsx.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return React.createElement(
+ "section",
+ {className: "App"},
+ React.createElement("h1", {}, "Hello, world!"),
+  )
+}
+
+export default App
