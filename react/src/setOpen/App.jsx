bc25c2f9709f806f57cc3207619529ddd3e66973
Premnath M <128115285+M-Premnath@users.noreply.github.com>
2024-12-26 20:15:45 +0530

    SetOpen

---
+++ react/src/setOpen/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const App = () => {
+    const [open, setOpen]= useState(false);
+  return (
+    <div>
+      <button onClick={()=> setOpen(true)}>Open</button>
+      {open &&(
+        <div>
+            <h1>Hello</h1>
+            <button onClick={()=> setOpen(false)}>❌</button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
