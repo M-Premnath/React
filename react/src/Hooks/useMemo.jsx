edf4b94291d8ce5469329cda75ca64292803cf1d
Premnath M <128115285+M-Premnath@users.noreply.github.com>
2024-12-30 19:52:13 +0530

    UseMemo

---
+++ react/src/Hooks/useMemo.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useMemo } from 'react'
+
+const App = () => {
+  const [add, setAdd] = useState(0);
+  const [sub, setSub] = useState(0);
+
+  let memoizedValue = useMemo(() => {
+    console.log("can you see me");
+    return add * 2;
+  }, [add]);
+  return (
+    <div>
+      <p>Add:{add}</p>
+      <p>Sub:{sub}</p>
+      <p>Mul:{memoizedValue}</p>
+      <button onClick={()=>setAdd(add + 1)}>Add</button>
+      <button onClick={()=>setSub(sub - 1)}>Sub</button>
+    </div>
+  )
+}
+
+export default App
