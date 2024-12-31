7d6b3dc5951c686eb362f3083f9578c279624877
Premnath M <128115285+M-Premnath@users.noreply.github.com>
2024-12-31 20:22:54 +0530

    routing css file added

--- react/src/Routing/Navbar.jsx
+++ react/src/Routing/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='navBar'>
         <Link to="/">Home</Link>
         <Link to="/contact">Contact</Link>
         <Link to ='/service'>Services</Link>
