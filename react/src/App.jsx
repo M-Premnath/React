b2d588a628352261b0e4ed631d2acf6fb93e8281
Premnath M <128115285+M-Premnath@users.noreply.github.com>
2025-01-10 09:26:11 +0530

    App.jsx default

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../src/Project 9 Axios/components/Home.jsx';
-import Create from '../src/Project 9 Axios/components/Create.jsx';
-import Edit from '../src/Project 9 Axios/components/Edit.jsx';
-import '../src/Project 9 Axios/main.css';
-import Navbar from '../src/Project 9 Axios/components/Navbar';
+import React from 'react'
+
 const App = () => {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar/>
-        <Routes>
-          {/* Define your routes here */}
-          <Route path="/" element={<Home />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/edit/:id" element={<Edit />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
